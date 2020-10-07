737d2df3d3bdcb4540a04c80164af49865cf25f8
Stefan <filimonstefanmihai@gmail.com>
2020-10-07 11:36:47 +0300

    Moved some things and added cool webhook in support server
    because webhooks are cool.

--- index.js
+++ index.js
@@ -91,9 +91,6 @@ client.on("message", async message => {
     }
     })
 
-// nice try stealing this
-client.login(process.env.BOT_TOKEN)
-
 // for discord bot lists
 // does this work? i really do not know
 const dbots = require("dbots")
@@ -107,3 +104,7 @@ const poster = new dbots.Poster({
 });
 
 poster.startInterval();
+
+
+// nice try stealing this
+client.login(process.env.BOT_TOKEN)
